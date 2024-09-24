056b5d8c428a99e65dab1a2b6bdfe89747299a83
gsaiteja2020 <gsaiteja2020@gmail.com>
2024-09-24 17:43:43 +0530

    oops

---
+++ Oops/Class/9-24-24.py
@@ -0,0 +1,17 @@
+
+class Product:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+    
+    def display(self):
+        print("product name is ",self.name)
+        print("product price is ",self.price)
+
+
+i=Product("iphone",49000)
+i.display()
+
+shirt=Product("netplay",1500)
+shirt.display()
+
